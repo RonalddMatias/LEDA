d49c4e2464bdfe6b4a0874a70f474cd2e04244cb
RonalddMatias <ronaldd.feliph.matias.costa@ccc.ufcg.edu.br>
2023-08-31 20:34:13 -0300

    Floor question done and refactoring quickSort algorithm

--- roteiros/R05-01-Rot-KLargestQuickSelectFloor-environment/src/main/java/problems/FloorBinarySearchImpl.java
+++ roteiros/R05-01-Rot-KLargestQuickSelectFloor-environment/src/main/java/problems/FloorBinarySearchImpl.java
@@ -8,17 +8,46 @@ public class FloorBinarySearchImpl implements Floor {
 
 	@Override
 	public Integer floor(Integer[] array, Integer x) {
-		QuickSort(array, 0, array.length-1);
-		// TODO implement your code here
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if(array.length > 0 && x != null){
+
+			QuickSort(array, 0, array.length-1);
+			System.out.println(Arrays.toString(array));
+
+			return binarySearch(array, x, null,0, array.length-1);
+		}
+		return null;
 	}
 
 
+	private int binarySearch(Integer[] array, Integer x, Integer floor, int leftIndex, int rightIndex) {
+		if((leftIndex >= 0)  && (rightIndex <= array.length) && (leftIndex != rightIndex) && (array.length > 0)){
+			if (leftIndex < rightIndex){
+				System.out.println(Arrays.toString(array));
+
+				int middle = (leftIndex + rightIndex) / 2;
+
+				// verificando se o meio é igual ao numero procurado
+				if(array[middle] == x){
+					return array[middle];
+				} else if (array[middle].compareTo(x) < 0){
+					int test = array[middle];
+					return binarySearch(array, x,array[middle], middle + 1, rightIndex); // olhando para a direita do array
+				} else {
+					return binarySearch(array, x, floor, leftIndex, middle - 1); // olhando para a esquerda do array
+				}
+		}
+	}	
+	return floor;
+}
+
+
 	public void QuickSort(Integer[] array, int leftIndex, int rightIndex) {
-		if(leftIndex < rightIndex){
+		if((leftIndex >= 0)  && (rightIndex <= array.length) && (leftIndex != rightIndex) && (array.length > 0)){
+			if(leftIndex < rightIndex){
 			int index_pivot = partition(array, leftIndex, rightIndex);
 			QuickSort(array, leftIndex, index_pivot-1);
 			QuickSort(array, index_pivot+1, rightIndex);
+			}
 		}
 	}
 
@@ -26,7 +55,7 @@ public class FloorBinarySearchImpl implements Floor {
 	// Algoritmo escolhido para ordenar o array para depois aplicar a busca binária.
 	public int partition(Integer[] array, int leftIndex, int rightIndex){
 		int pivot = array[leftIndex]; // escolhendo o pivot com sendo sempre a primeira posiçao
-		int i = 0;
+		int i = leftIndex;
 
 		for (int j = leftIndex + 1; j <= rightIndex; j++) {
 			if(array[j].compareTo(pivot) < 0){
@@ -41,9 +70,8 @@ public class FloorBinarySearchImpl implements Floor {
 
 	public static void main(String[] args) {
 		FloorBinarySearchImpl floorBinarySearchImpl = new FloorBinarySearchImpl();
-		Integer[] num = new Integer[]{4, 10, 2, 0, -21, 12};
-		floorBinarySearchImpl.floor(num, 2);
-		System.out.println(Arrays.toString(num));
+		Integer[] num = new Integer[]{4,15,25,30,38};
+		System.out.println(floorBinarySearchImpl.floor(num, 3));
 	}
 
 }
