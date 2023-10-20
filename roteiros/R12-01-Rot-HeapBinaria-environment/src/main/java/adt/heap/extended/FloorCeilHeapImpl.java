34d99c4594db168e557e0e393e87eb3bd05cc28b
RonalddMatias <ronaldd.feliph.matias.costa@ccc.ufcg.edu.br>
2023-10-19 22:58:56 -0300

    adding ceil and floor

--- roteiros/R12-01-Rot-HeapBinaria-environment/src/main/java/adt/heap/extended/FloorCeilHeapImpl.java
+++ roteiros/R12-01-Rot-HeapBinaria-environment/src/main/java/adt/heap/extended/FloorCeilHeapImpl.java
@@ -12,14 +12,52 @@ public class FloorCeilHeapImpl extends HeapImpl<Integer> implements FloorCeilHea
 
 	@Override
 	public Integer floor(Integer[] array, double numero) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		Integer output = null;
+		if (array != null){
+			for (Integer number : array) {
+				insert(number);
+			}
+			output = findFloor(numero, null);
+		}
+		return output;
+	}
+
+	private Integer findFloor(double numero, Integer floor) {
+		Integer root =  extractRootElement(); // elemento
+		if(root != null){
+			boolean verifica = floor == null || root >= floor; // essa segunda verificação segura para quando nos acharmos o primeiro floor
+			if(verifica && numero >= root){
+				floor = findFloor(numero, root);
+			} else {
+				floor = findFloor(numero, floor);
+			}
+		}
+		return floor; // so vai parar quando o node for null
 	}
 
 	@Override
 	public Integer ceil(Integer[] array, double numero) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		Integer output = null;
+		if(array != null){
+			for (Integer number: array) {
+				insert(number);
+			}
+			output = findCeil(numero, null);
+		}
+		return output;
+	}
+
+	private Integer findCeil(double numero, Integer ceil) {
+		Integer root = extractRootElement();
+		if(root != null){
+			boolean verifica = ceil == null || root <= numero;
+			if(verifica && numero <= root){
+				ceil = findCeil(numero, root);
+			} else {
+				ceil = findCeil(numero, ceil);
+			}
+		}
+		return ceil;
 	}
 
 }
